8741ec4c8dd5bdf1f50bee448b17320cf3f6d1f0
Coptic <justicejacobs2@gmail.com>
2022-01-24 21:05:17 -0500

    comment

---
+++ Hello_World/HelloWorld.java
@@ -0,0 +1,17 @@
+/* Example Hello World Program in Java */
+
+class Example2 {
+  public static void main(String[] args) {
+
+    int myVar1 = 1 , myVar2 = 2;
+
+    int count;
+
+    for(count = 0; count < 5; count = count + 1)
+      System.out.println("Count: " + count);
+
+    
+
+  }
+
+}
